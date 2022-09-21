65c2191b30b017e96e4a8f8b0865ae98c736eb2c
PandaBearBar <97035109+PandaBearBar@users.noreply.github.com>
2022-09-21 20:31:04 +0300

    Update WebConfig.java

--- src/main/java/com/jb/project3/finalCouponSystem/config/WebConfig.java
+++ src/main/java/com/jb/project3/finalCouponSystem/config/WebConfig.java
@@ -13,43 +13,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Configuration
-public class WebConfig implements Filter {
+public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry
-//                .addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("*")
-//                .allowedHeaders("*");
-//    }
-    @Bean
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }    @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
-    }
-
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        ((HttpServletResponse)servletResponse).addHeader("Access-Control-Allow-Origin","*");
-        ((HttpServletResponse)servletResponse).addHeader("Access-Control-Allow-Origin","GET, OPTIONS, HEAD, PUT, DELETE, *");
-        ((HttpServletResponse)servletResponse).addHeader("Access-Control-Allow-Origin","authorization, Origin, *");
-
-        HttpServletResponse resp = (HttpServletResponse) servletResponse;
-        if (request.getMethod().equals("OPTIONS")){
-            return;
-        }
-        filterChain.doFilter(request,servletResponse);
-    }
-
-    @Override
-    public void destroy() {
-        Filter.super.destroy();
     }
 }
