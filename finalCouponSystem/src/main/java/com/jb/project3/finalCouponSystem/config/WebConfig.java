01e626722964d550ad2616ed58d9895c1a741393
PandaBearBar <97035109+PandaBearBar@users.noreply.github.com>
2022-09-21 17:00:30 +0300

    updates

--- finalCouponSystem/src/main/java/com/jb/project3/finalCouponSystem/config/WebConfig.java
+++ finalCouponSystem/src/main/java/com/jb/project3/finalCouponSystem/config/WebConfig.java
@@ -7,19 +7,49 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
-    }
+@Configuration
+public class WebConfig implements Filter {
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry
+//                .addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("*")
+//                .allowedHeaders("*");
+//    }
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        ((HttpServletResponse)servletResponse).addHeader("Access-Control-Allow-Origin","*");
+        ((HttpServletResponse)servletResponse).addHeader("Access-Control-Allow-Origin","GET, OPTIONS, HEAD, PUT, DELETE, *");
+        ((HttpServletResponse)servletResponse).addHeader("Access-Control-Allow-Origin","authorization, Origin, *");
+
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        if (request.getMethod().equals("OPTIONS")){
+            return;
+        }
+        filterChain.doFilter(request,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
     }
 }
